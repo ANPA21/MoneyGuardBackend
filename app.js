1748ce4b2b663c39b80964135a7a3e226ddca0d5
Andrew <papsujevicandrew@gmail.com>
2023-08-31 17:31:06 +0300

    error handling fix

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  if (err.status) {
+    res.status(err.status).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports = app;
