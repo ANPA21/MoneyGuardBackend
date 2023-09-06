cc2034f2f4163182fa0491ce2dbb30de086326bc
Andrew <papsujevicandrew@gmail.com>
2023-09-06 22:27:00 +0300

    add/edit transactions fix date
    fixed date key creation and editing in transactions objects in DB

--- controllers/transactions/updateTransactions.js
+++ controllers/transactions/updateTransactions.js
@@ -1,12 +1,18 @@
-const Transaction = require("../../models/transactions.model");
+const Transaction = require('../../models/transactions.model');
 
-const { requestError } = require("../../helpers");
+const { requestError } = require('../../helpers');
 
 const updateTransactions = async (req, res) => {
   const { id } = req.params;
-  const result = await Transaction.findByIdAndUpdate(id, req.body, { new: true });
+  if (req.body.type === 'income') {
+    await Transaction.findByIdAndUpdate(id, { $unset: { category: 1 } });
+  }
+  result = await Transaction.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
   if (!result) {
-    throw requestError(404, "Not found");
+    throw requestError(404, 'Not found');
   }
   res.status(201).json(result);
 };
--- schemas/transactionsSchemas/transactionSchema.js
+++ schemas/transactionsSchemas/transactionSchema.js
@@ -1,52 +1,52 @@
-const { Schema } = require("mongoose");
+const { Schema } = require('mongoose');
 
-const { handleSaveErrors } = require("../../helpers");
+const { handleSaveErrors } = require('../../helpers');
 
 const transactionSchema = new Schema(
   {
     comment: {
       type: String,
-      default: "",
+      default: '',
     },
     owner: {
       type: Schema.Types.ObjectId,
-      ref: "user",
+      ref: 'user',
       required: true,
     },
     type: {
       type: String,
-      enum: ["expense", "income"],
-      default: "income",
+      enum: ['expense', 'income'],
+      default: 'income',
     },
     category: {
       type: String,
-      ref: "category",
+      ref: 'category',
       required: function () {
-        return this.type === "expense";
+        return this.type === 'expense';
       },
       enum: [
-        "Products",
-        "Car",
-        "Main expenses",
-        "Education",
-        "Entertainment",
-        "Self care",
-        "Leisure",
-        "Other expenses",
-        "Education",
-        "Household products",
+        'Products',
+        'Car',
+        'Main expenses',
+        'Education',
+        'Entertainment',
+        'Self care',
+        'Leisure',
+        'Other expenses',
+        'Education',
+        'Household products',
       ],
     },
     value: {
       type: Number,
       min: 0,
-      required: [true, "Funds value is required"],
+      required: [true, 'Funds value is required'],
     },
+    date: { type: Date, required: true },
   },
   { versionKey: false, timestamps: true }
 );
 
-transactionSchema.post("save", handleSaveErrors);
+transactionSchema.post('save', handleSaveErrors);
 
 module.exports = transactionSchema;
-
